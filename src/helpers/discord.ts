e020d1545f84264470b204895813c0e900a5a5ae
Jay Montal <Jaedwin.Montal@gmail.com>
2021-06-14 21:16:42 -0600

    Hotfix path in discord.ts file

--- src/helpers/discord.ts
+++ src/helpers/discord.ts
@@ -1,22 +1,22 @@
 import { Client, Channel, Message, Collection } from 'discord.js';
 import fs from 'fs';
 
-export const initDiscordClient = (token:string) : Client => {
+export const initDiscordClient = (token: string): Client => {
     const discordClient = new Client();
     discordClient.login(token);
     return discordClient;
 }
 
-export const getCommandFiles = (discordClient:any, path:string) : Array<String> => {
+export const getCommandFiles = (discordClient: any, path: string): Array<String> => {
     discordClient.commands = new Collection();
-    return fs.readdirSync(path + "\\commands").filter(file => file.endsWith('.ts'));
+    return fs.readdirSync(path + "/commands").filter(file => file.endsWith('.ts'));
 }
 
-export const getChannel = (discordClient:Client, msg:Message) : Channel => {
+export const getChannel = (discordClient: Client, msg: Message): Channel => {
     const channelID = msg.member?.voice.channelID;
     return discordClient.channels.cache.get(channelID!)!;
 }
 
-export const getUsers = (channel:any) : Array<String> => {
+export const getUsers = (channel: any): Array<String> => {
     return channel.members.map((member: any) => member.user);
 }
