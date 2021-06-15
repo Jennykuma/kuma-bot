c1f4ae86f1db690ae62b48546268b0b1ec002a16
Sam Osman <sam.osman97@gmail.com>
2021-06-15 17:59:56 -0600

    fix(*) | using path to join directory names

--- src/helpers/discord.ts
+++ src/helpers/discord.ts
@@ -1,5 +1,6 @@
 import { Client, Channel, Message, Collection } from 'discord.js';
 import fs from 'fs';
+import p from 'path';
 
 export const initDiscordClient = (token: string): Client => {
     const discordClient = new Client();
@@ -9,7 +10,7 @@ export const initDiscordClient = (token: string): Client => {
 
 export const getCommandFiles = (discordClient: any, path: string): Array<String> => {
     discordClient.commands = new Collection();
-    return fs.readdirSync(path + "/commands").filter(file => file.endsWith('.ts'));
+    return fs.readdirSync(p.join(path, 'commands')).filter(file => file.endsWith('.ts'));
 }
 
 export const getChannel = (discordClient: Client, msg: Message): Channel => {
--- src/kuma-bot.ts
+++ src/kuma-bot.ts
@@ -2,22 +2,22 @@ import ENV from './config';
 import { prefix } from './static_data.json';
 import { initDiscordClient, getCommandFiles } from './helpers/discord';
 
-const discordClient : any = initDiscordClient(ENV.TOKEN);
+const discordClient: any = initDiscordClient(ENV.TOKEN);
 const commandFiles = getCommandFiles(discordClient, __dirname);
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	discordClient.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  discordClient.commands.set(command.name, command);
 }
 
 discordClient.on('ready', () => {
   console.log(`Logged in as ${discordClient?.user?.tag}!`);
 });
 
-discordClient.on('message', (msg:any) => {
+discordClient.on('message', (msg: any) => {
   if (!msg.content.toLowerCase().startsWith(prefix) || msg.author.bot) return;
 
-	const args = msg.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
   if (!discordClient.commands.has(command)) return;
 
